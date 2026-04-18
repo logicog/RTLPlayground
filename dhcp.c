6c6d11ab33c36762767bc2be43e03718aac6c87b
René van Dorst <opensource@vdorst.com>
2026-04-18 21:56:29 +0200

    dhcp: de-__gptrget()-call in dhcp_print_ip()

--- dhcp.c
+++ dhcp.c
@@ -82,7 +82,7 @@ struct dhcp_pkt {
 __xdata uint32_t long_value;
 
 
-void dhcp_print_ip(uint8_t *a)
+void dhcp_print_ip(__xdata uint8_t *a)
 {
 	itoa(a[0]); write_char('.');
 	itoa(a[1]); write_char('.');
