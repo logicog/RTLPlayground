facc4afe400df439e397490ae437e5bd685e9966
René van Dorst <opensource@vdorst.com>
2026-04-18 21:48:55 +0200

    uip: de-__gptrget()-call in timer_{set,reset,restart,exprired}()

--- uip/timer.h
+++ uip/timer.h
@@ -76,10 +76,10 @@ struct timer {
   clock_time_t interval;
 };
 
-void timer_set(struct timer *t, clock_time_t interval);
-void timer_reset(struct timer *t);
-void timer_restart(struct timer *t);
-int timer_expired(struct timer *t);
+void timer_set(__xdata struct timer *t, clock_time_t interval);
+void timer_reset(__xdata struct timer *t);
+void timer_restart(__xdata struct timer *t);
+int timer_expired(__xdata struct timer *t);
 
 #endif /* __TIMER_H__ */
 
