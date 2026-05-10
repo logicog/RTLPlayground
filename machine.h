a1135863bb03daa46033e899ca42ab2438df39f1
feelfree69 <github@bkwn.de>
2026-05-10 12:16:48 +0200

    add MACHINE_KP_9000_6XH_X2

--- machine.h
+++ machine.h
@@ -8,6 +8,7 @@
  */
 // #define MACHINE_KP_9000_6XHML_X2
 // #define MACHINE_KP_9000_6XH_X
+// #define MACHINE_KP_9000_6XH_X2
 // #define MACHINE_KP_9000_9XH_X_EU
 // #define MACHINE_KP_9000_9XHML_X_V2_2
 // #define MACHINE_KP_9000_9XHML_X_V3_1
