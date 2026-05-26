logicog/RTLPlayground

commit 2cdab5f9d903826683aad83dd99cbf4c9aefdce5

parse_reg{set,get}() make use cmd_words_len.

cmd_parser.c
@@ -737,7 +737,7 @@ void parse_regget(void)
 {
 	uint16_t reg = 0;
 
-	if (cmd_words_b[1] < 0) {
+	if (cmd_words_len != 2) {
 		goto err;
 	}
 
@@ -772,7 +772,7 @@ void parse_regset(void)
 {
 	uint16_t reg = 0;
 
-	if (cmd_words_b[2] < 0) {
+	if (cmd_words_len != 2) {
 		goto err;
 	}
 

commit 005eaa3248f4e4269bea6acc0fd800c9440e9f7a

Correct argument count for regset command
Fixes: 2cdab5f9d9038266 ("parse_reg{set,get}() make use cmd_words_len.")

cmd_parser.c
@@ -787,7 +787,7 @@ void parse_regset(void)
 {
 	uint16_t reg = 0;
 
-	if (cmd_words_len != 2) {
+	if (cmd_words_len != 3) {
 		goto err;
 	}
 