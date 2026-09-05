cb65559578b39e87df152753baa026868a562b60
Rob Emery <git@mintsoft.net>
2026-09-05 10:29:56 +0100

    Cleaning out unused functions now it's all inlined

--- lldp.h
+++ lldp.h
@@ -11,10 +11,6 @@
 
 void lldp_init();
 void lldp_tick(void);
-uint16_t put_eth_header(uint8_t *p);
-uint16_t lldp_put_tlv(uint8_t *p, uint8_t type, const uint8_t *value, uint8_t value_len);
-uint16_t lldp_put_local_string_tlv(uint8_t *p, uint8_t type, const char *string);
-uint16_t lldp_put_ttl_tlv(uint8_t *p, uint16_t ttl);
 void lldp_send(void);
 
 #endif
