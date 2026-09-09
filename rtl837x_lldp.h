3a7d08c8e09a9fb706f2b39c3388f342fc640b7b
Rob Emery <git@mintsoft.net>
2026-09-09 15:48:14 +0100

    Refactoring a bit and make the type values hopefully clearer

--- rtl837x_lldp.h
+++ rtl837x_lldp.h
@@ -8,8 +8,25 @@
 #define LLDP_MAC_ADDR_LEN    6
 #define LLDP_TX_INTERVAL_SEC 30
 
-void lldp_init(void);
-void lldp_tick(void);
-void lldp_send(void);
+#define LLDP_PORT_ID_TLV_TYPE 2
+#define LLDP_PORT_ID_TLV_LENGTH 0x02
+#define LLDP_PORT_ID_TLV_SUBTYPE 0x07
+
+#define LLDP_TTL_TLV_TYPE 3
+#define LLDP_TTL_TLV_LENGTH 2
+#define LLDP_TTL_TTL_SECONDS 120
+
+#define LLDP_CHASSIS_ID_TLV_TYPE 1
+#define LLDP_CHASSIS_ID_TLV_LENGTH 7
+#define LLDP_CHASSIS_ID_TLV_SUBTYPE 4
+
+#define LLDP_SYSNAME_TLV_TYPE 5
+#define LLDP_SYSDESC_TLV_TYPE 6
+
+void lldp_init(void) __banked;
+void lldp_tick(void) __banked;
+void lldp_send(void) __banked;
+void lldp_set_addresses(void) __banked;
+void lldp_set_rtl_wrapper(void) __banked;
 
 #endif
